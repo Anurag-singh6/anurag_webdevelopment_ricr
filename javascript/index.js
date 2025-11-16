b1c745ba228bd71379e270ce2b96187722c162a0
Anurag-singh6 <anuragsingh76000@gmail.com>
2025-11-16 16:00:48 +0530

    bootstrap login register form complete

---
+++ javascript/index.js
@@ -0,0 +1,16 @@
+var a = 5;
+var b = 20;
+
+console.log(a, b);
+
+let ar = ["abc", 1234, 3.14, "c", true];
+console.log(ar);
+
+console.warn("yo yo");
+console.error("oops sory");
+// alert("Hello World");
+if (confirm("buy some material")) {
+  console.log("chai samosa");
+} else {
+  console.log("nothing");
+}
