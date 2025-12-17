5b14af1770cf54e7e274f38dfb30de073829b47d
Anurag-singh6 <anuragsingh76000@gmail.com>
2025-12-17 12:28:56 +0530

    update in form validation

--- javascript/form_validation/script.js
+++ javascript/form_validation/script.js
@@ -4,19 +4,49 @@ function submit() {
   const e = document.getElementById("em").value.trim();
   const b = document.getElementById("dob").value.trim();
 
-  if (!/^[A-Zaa-z]+$/.test(n)) {
-    alert("wrong input name");
+  document.querySelectorAll(".Error").forEach((element) => {
+    element.innerHTML = "";
+  });
+
+  // validation start
+  if (!n) {
+    document.getElementById("NameError").innerText = "Required";
     return;
-  }
-  if (!/^[\w\.]+@(gmail|outlook|ricr|yahoo)\.(com|in)$/.test(e)) {
-    alert("wrong email");
-    return;
-  }
-  if (!/^[6-9]\d{9}$/.test(m)) {
-    alert("wrong number");
+  } else if (!/^[A-Za-z ]+$/.test(n)) {
+    document.getElementById("NameError").innerText =
+      "Only Alphabets and Spaces are Allowed";
     return;
   }
 
+  if (!e) {
+    document.getElementById("EmailError").innerText = "Required";
+    return;
+  } else if (!/^[\w\.]+@(gmail|outlook|ricr|yahoo)\.(com|in|co.in)$/.test(e)) {
+    document.getElementById("EmailError").innerText = "Use proper Email Format";
+    return;
+  }
+
+  if (!m) {
+    document.getElementById("PhoneError").innerText = "Required";
+    return;
+  } else if (!/^[6-9]\d{9}$/.test(m)) {
+    document.getElementById("PhoneError").innerText =
+      "Only Indian Mobile Number Allowed";
+    return;
+  }
+
+  if (!b) {
+    document.getElementById("BirthError").innerText = "Required";
+    return;
+  } else {
+    const curryear = new Date().getFullYear();
+    const birthyear = Number(b.split("-")[0]);
+    if (curryear - birthyear < 17) {
+      document.getElementById("BirthError").innerText =
+        "You must be of 18 years Old";
+      return;
+    }
+  }
   const data = {
     fullname: n,
     mobno: m,
