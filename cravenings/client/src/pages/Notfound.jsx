4d53b29fe7f7acd9531de5abe5fefb8404efcb5d
Anurag-singh6 <anuragsingh76000@gmail.com>
2026-02-14 13:52:33 +0530

    data add through seeders

--- cravenings/client/src/pages/Notfound.jsx
+++ cravenings/client/src/pages/Notfound.jsx
@@ -15,10 +15,10 @@ const Notfound = () => {
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <button className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={()=>navigate("/")}>
+            <button className="rounded-md bg-indigo-600 cursor-pointer px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={()=>navigate("/")}>
               Go back home
             </button>
-            <button className="text-sm font-semibold text-gray-900" onClick={()=>navigate("/contact")}>
+            <button className="px-3.5 py-2.5 rounded-md text-sm cursor-pointer hover:underline hover:bg-gray-300 font-semibold text-gray-900" onClick={()=>navigate("/contact")}>
               Contact support <span aria-hidden="true">&rarr;</span>
             </button>
           </div>
