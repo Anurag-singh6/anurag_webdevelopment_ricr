c28efe257137119d450256e0b7843fc93abb0f0b
Anurag-singh6 <anuragsingh76000@gmail.com>
2026-03-22 15:39:25 +0530

    done

--- cravenings/client/src/config/Api.jsx
+++ cravenings/client/src/config/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4500",
+  baseURL: "https://crave-backend-rwxi.onrender.com",
   withCredentials: true,
 });
 
